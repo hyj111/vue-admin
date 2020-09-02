8a6d098865335db312730e03803f378f0eb468a0
hyj111 <1069234622@qq.com>
2020-09-02 14:39:58 +0800

    登录页面开发，验证码接口的调试

--- src/components/public/sign/sign.vue
+++ src/components/public/sign/sign.vue
@@ -14,11 +14,17 @@
       </el-form-item>
 
       <el-form-item label="密码" prop="password">
-        <el-input type="password" v-model="ruleForm.password" autocomplete="off" minlength="6" maxlength="12"></el-input>
+        <el-input
+          type="password"
+          v-model="ruleForm.password"
+          autocomplete="off"
+          minlength="6"
+          maxlength="12"
+        ></el-input>
       </el-form-item>
 
       <el-form-item label="确认密码" prop="checkPass" v-if="currentIndex!=0">
-         <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+        <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
       </el-form-item>
 
       <el-form-item label="验证码" prop="code">
@@ -27,38 +33,50 @@
             <el-input v-model.number="ruleForm.code"></el-input>
           </el-col>
           <el-col :span="12">
-            <el-button type="success" class="verification" minlength="6" maxlength="6">验证码</el-button>
+            <el-button
+              type="success"
+              class="verification"
+              minlength="6"
+              maxlength="6"
+              @click="getSms"
+            >验证码</el-button>
           </el-col>
         </el-row>
       </el-form-item>
       <el-form-item>
-        <el-button type="danger" @click="submitForm('ruleForm')" class="block">提交</el-button>
+        <el-button
+          type="danger"
+          @click="submitForm('ruleForm')"
+          class="block"
+          :disabled="true"
+        >{{currentIndex==0?'登录':'注册'}}</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script>
-import {stripscript} from "@/utils/validate"
+import { GetSms } from "@/api/login.js";
+import { stripscript } from "@/utils/validate";
 export default {
   name: "Sign",
-  props:{
-      currentIndex:{
-          type:Number,
-          default:0
-      }
+  props: {
+    currentIndex: {
+      type: Number,
+      default: 0
+    }
   },
   data() {
-      //验证码规则
+    //验证码规则
     var alidateCode = (rule, value, callback) => {
-          this.ruleForm.code = stripscript(value)
-        value = this.ruleForm.code
-    let reg = /^[a-z0-9]{6}$/
+      this.ruleForm.code = stripscript(value);
+      value = this.ruleForm.code;
+      let reg = /^[a-z0-9]{6}$/;
       if (!value) {
         return callback(new Error("请输入验证码"));
-      }else if (!reg.test(value)) {
+      } else if (!reg.test(value)) {
         callback(new Error("验证码格式不正确"));
-      }else {
+      } else {
         callback();
       }
     };
@@ -77,9 +95,9 @@ export default {
 
     //密码规则
     var validatePassword = (rule, value, callback) => {
-        this.ruleForm.password = stripscript(value)
-        value = this.ruleForm.password
-    let reg = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,12}$/
+      this.ruleForm.password = stripscript(value);
+      value = this.ruleForm.password;
+      let reg = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,12}$/;
       if (value === "") {
         callback(new Error("请输入密码"));
       } else if (!reg.test(value)) {
@@ -90,14 +108,14 @@ export default {
     };
     //验证2次密码word
     var validatePass2 = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('请再次输入密码'));
-        } else if (value !== this.ruleForm.password) {
-          callback(new Error('两次输入密码不一致!'));
-        } else {
-          callback();
-        }
-      };
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.ruleForm.password) {
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    };
     return {
       ruleForm: {
         username: "",
@@ -108,9 +126,12 @@ export default {
         username: [{ validator: validateUsername, trigger: "blur" }],
         password: [{ validator: validatePassword, trigger: "blur" }],
         code: [{ validator: alidateCode, trigger: "blur" }],
-        checkPass: [{ validator: validatePass2, trigger: 'blur' }],
+        checkPass: [{ validator: validatePass2, trigger: "blur" }]
       }
     };
+  },
+  computed: {
+    
   },
   methods: {
     submitForm(formName) {
@@ -122,6 +143,23 @@ export default {
           return false;
         }
       });
+    },
+    // 获取验证码
+    getSms() {
+      let reg = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+      if(this.ruleForm.username == ''){
+        this.$message.error('邮箱不能为空')
+        return false
+      }else if(reg.test(this.ruleForm.username)==false){
+        this.$message.error('邮箱格式错误')
+         return false
+      }
+      let requestData = { username: this.ruleForm.username,module:'login'}
+      GetSms(requestData).then(res=>{
+        console.log(res);
+      }).catch(err=>{
+        console.log(err);
+      })
     }
   }
 };
